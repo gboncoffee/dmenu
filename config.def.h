d4ebb99f111d42ea87900ae0276f7aea4faedacd
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-06-10 12:19:37 -0300

    Revert "Added light colorscheme"
    This reverts commit c7ddd7958c52a075685837481fc300d8ce573903.

--- config.def.h
+++ config.def.h
@@ -7,7 +7,12 @@ static const char *fonts[] = {
 	"CaskaydiaCove Nerd Font:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-#include"colors.h"
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#CDD6F4", "#1E1E2E" },
+	[SchemeSel] = { "#1E1E2E", "#F5C2E7" },
+	[SchemeOut] = { "#1E1E2E", "#89B4FA" },
+};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
--- dark.h
+++
@@ -1,8 +0,0 @@
-/* Catppuccin Mocha (dark) */
-
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#CDD6F4", "#1E1E2E" },
-	[SchemeSel] = { "#1E1E2E", "#F5C2E7" },
-	[SchemeOut] = { "#1E1E2E", "#89B4FA" },
-};
