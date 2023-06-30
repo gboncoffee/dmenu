533fb04188698d3593966ae9b7f812e0a7dd2ba4
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-06-30 09:22:08 -0300

    Added grid and gridnav

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"CaskaydiaCove Nerd Font:size=15"
+	"CaskaydiaCove Nerd Font:size=20"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -14,7 +14,9 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = {  "#282a36", "#ff79c6" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 10;
+static unsigned int columns    = 3;
 
 /*
  * Characters not considered part of a word while deleting words
